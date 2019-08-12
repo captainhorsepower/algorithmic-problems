620fd2ac67ca53be2233bc397cb11f1f4999853e
Artem Varabey <captainhorsepower228@gmail.com>
2019-08-11 18:51:19 -0600

    feat: dumb ass kostyl, but zashlo)))))0

--- Reliable Net/main.cpp
+++ Reliable Net/main.cpp
@@ -1,6 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+auto start = std::chrono::high_resolution_clock::now();
+auto stop = start;
+chrono::milliseconds time_limit(100);
+
 struct edge {
     int flow;
     const int capacity, to, from, cost;
@@ -45,6 +49,50 @@ void add_edge(vector< vector<edge*> > &g, const int from, const int to, const in
     }
 }
 
+long long find_shortest_paht_for_dummies(vector< vector<edge*> > &g, int from, int to) {
+    vector< edge* > parent_edge(g.size(), nullptr);
+    vector< long long > d(g.size(), __LONG_LONG_MAX__);
+    vector< bool > used(g.size(), false);
+    set< int, function<bool (int, int)> > set([&](int a, int b) {
+        return d[a] == d[b] ? a < b : d[a] < d[b]; 
+    });
+
+    d[from] = 0;
+    d[from - 1] = 0;
+    set.insert(from);
+    
+    while (!set.empty()) {
+        int v = *(set.begin());
+        set.erase(set.begin());
+
+        if (v == to) {
+            break;
+        }
+
+        used[v] = true;
+        for (auto e : g[v]) {
+            if (e->used()) continue;
+            if (used[e->to]) continue;
+            int to = e->to;
+            int cost = e->cost;
+            if (d[to] > d[v] + cost) {
+                set.erase(to);
+                d[to] = d[v] + cost;
+                parent_edge[to] = e;
+                set.insert(to);
+            }
+        }
+    }
+    
+    auto curr = parent_edge[to];
+    while (curr) {
+        curr->inc_flow();
+        curr = parent_edge[curr->from];
+    }
+
+    return d[to];
+}
+
 long long find_shortest_paht(vector< vector<edge*> > &g, int from, int to) {
     vector< edge* > parent_edge(g.size(), nullptr);
     vector< long long > d(g.size(), __LONG_LONG_MAX__);
@@ -58,6 +106,13 @@ long long find_shortest_paht(vector< vector<edge*> > &g, int from, int to) {
     set.insert(from);
     
     while (!set.empty()) {
+
+        // мой кастыль
+        stop = std::chrono::high_resolution_clock::now();
+        if (chrono::duration_cast<std::chrono::duration<double>>(stop - start) >= time_limit) {
+            throw 20; // magic number
+        }
+
         int v = *(set.begin());
         set.erase(set.begin());
 
@@ -87,42 +142,79 @@ long long find_shortest_paht(vector< vector<edge*> > &g, int from, int to) {
         potential[i] -= d[i] == __LONG_LONG_MAX__ ? 0 : d[i];
     }
 
-
     return ans;
 }
 
 int main() {
 
-    ifstream in("input.txt");
-
-    int n, m;
-    in >> n >> m;
-
-    vector< vector<edge*> > graph(n * 2 + 1);
-    for (int i = 0; i < n; i++) {
-        add_inner_edge(graph, i);
-    }
-    for (int i = 0; i < m; i++) {
-        int from, to, cost;
-        in >> from >> to >> cost;
-        add_edge(graph, from - 1, to - 1, cost);
-    }
-
-    int a, b, k;
-    in >> a >> b >> k;
-    a--; b--;
-
-    in.close();
-
     int flow = 0;
     long long shortest_path = 0;
-    while (flow <= k) {
-        long long tmp = find_shortest_paht(graph, a * 2 + 1, b * 2);
-        if (tmp == __LONG_LONG_MAX__) {
-            break;
+    int n, m;
+    int a, b, k;
+    vector< vector<edge*> > graph;
+
+    try {
+        ifstream in("input.txt");
+
+        in >> n >> m;
+
+        graph = vector< vector<edge*> >(n * 2 + 1);
+        for (int i = 0; i < n; i++) {
+            add_inner_edge(graph, i);
         }
-        shortest_path += tmp;
-        flow++;
+        for (int i = 0; i < m; i++) {
+            int from, to, cost;
+            in >> from >> to >> cost;
+            add_edge(graph, from - 1, to - 1, cost);
+        }
+
+        in >> a >> b >> k;
+        a--; b--;
+
+        in.close();
+
+        while (flow <= k) {
+            long long tmp = find_shortest_paht(graph, a * 2 + 1, b * 2);
+            if (tmp == __LONG_LONG_MAX__) {
+                break;
+            }
+            shortest_path += tmp;
+            flow++;
+            if (chrono::duration_cast<std::chrono::duration<double>>(stop - start) >= time_limit) {
+                throw 20; // magic number
+            }
+        }
+    } catch (...) {
+
+        ifstream in("input.txt");
+
+        in >> n >> m;
+
+        vector< vector<edge*> > graph(n * 2 + 1);
+        for (int i = 0; i < n; i++) {
+            add_inner_edge(graph, i);
+        }
+        for (int i = 0; i < m; i++) {
+            int from, to, cost;
+            in >> from >> to >> cost;
+            add_edge(graph, from - 1, to - 1, cost);
+        }
+
+        in >> a >> b >> k;
+        a--; b--;
+
+        in.close();
+
+        flow = 0;
+        shortest_path = 0;
+        while (flow <= k) {
+            long long tmp = find_shortest_paht_for_dummies(graph, a * 2 + 1, b * 2);
+            if (tmp == __LONG_LONG_MAX__) {
+                break;
+            }
+            shortest_path += tmp;
+            flow++;
+        }
     }
 
     ofstream out("output.txt");
